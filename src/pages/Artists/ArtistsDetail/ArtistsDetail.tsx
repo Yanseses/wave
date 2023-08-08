0923e3523844532bd439e2afa3633f0148559a71
Yanseses <s0dom546@gmail.com>
2023-08-08 17:59:24 +0300

    changeReadme

--- src/pages/Artists/ArtistsDetail/ArtistsDetail.tsx
+++ src/pages/Artists/ArtistsDetail/ArtistsDetail.tsx
@@ -3,8 +3,9 @@ import { useLocation } from "react-router";
 
 export const ArtistsDetail: FC = () => {
   const location = useLocation();
+  const artist = decodeURI(location.state.artist).split('-').join(' ');
 
   return (
-    <div>{ location.state.artist }</div>  
+    <div>{ artist }</div>  
   )
 }
